b8922062bbc4196a74d442d2f17ec5ca58fb363f
Plotvochka <106927858+Plotvochka@users.noreply.github.com>
2024-09-12 16:05:20 +0300

    components and page

---
+++ src/components/Service/movieReviews-api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const options = {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmMwYzU1NTkyM2IzNjk0NWNlNmYxMzE4YTJiNjhjOCIsIm5iZiI6MTcyNjEzMjQ4NS4yMjMwNTQsInN1YiI6IjY2ZTJhYzdlYzgxYjI0YjNmZTIzN2ZlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wCX5EY_vgFQvzd7JH_cbtQdtciPEPX66Y-jGqEfhIN4",
+  },
+};
+axios.defaults.baseURL = "https://api.themoviedb.org";
+
+export default async function getMovieReviews(id) {
+  const response = await axios.get(
+    `/3/movie/${id}/reviews?language=en-US`,
+    options
+  );
+  return response.data.results;
+}
---
+++ src/components/Service/searchMovie-api.js
@@ -0,0 +1,18 @@
+"use strict";
+import axios from "axios";
+
+const options = {
+  headers: {
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYmMwYzU1NTkyM2IzNjk0NWNlNmYxMzE4YTJiNjhjOCIsIm5iZiI6MTcyNjEzMjQ4NS4yMjMwNTQsInN1YiI6IjY2ZTJhYzdlYzgxYjI0YjNmZTIzN2ZlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wCX5EY_vgFQvzd7JH_cbtQdtciPEPX66Y-jGqEfhIN4",
+  },
+};
+axios.defaults.baseURL = "https://api.themoviedb.org";
+
+export default async function getSearchMovie(query) {
+  const response = await axios.get(
+    `3//search/movie?query=${query}&include_adult=false&language=en-US&page=1`,
+    options
+  );
+  return response.data;
+}
